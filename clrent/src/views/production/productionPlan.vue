8da5e679b3e32421e0d0f2a97922c6fcc3993d8b
poes0147 <104211147+poes0147@users.noreply.github.com>
2025-04-28 17:56:14 +0900

    0428 리뷰

--- clrent/src/views/production/productionPlan.vue
+++ clrent/src/views/production/productionPlan.vue
@@ -110,9 +110,9 @@
           st_dt: '',
           end_dt: '',
           rmk: '',
-          details:[
-            {pro_no:'', qry:''}
-          ]
+          // details:[
+          //   {pro_no:'', qry:''}
+          // ]
         },
         showModal: false,
             inventoryList: [
