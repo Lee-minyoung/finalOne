c882b963c7aaa2d9eb25c154d30d2eb2042183d7
Lee min young <lmy1147@naver.com>
2025-05-02 17:11:02 +0900

    0502

--- clrent/src/views/qualitys/spmInsStd.vue
+++ clrent/src/views/qualitys/spmInsStd.vue
@@ -7,59 +7,14 @@
     </div>
     <!-- 조회 조건 -->
     <div class="input">
-      제품번호 <input class="form-control" id="input_id" placeholder="" v-model="searchQuery"/>
-      <button type="button" class="icon-btn" data-bs-toggle="modal" data-bs-target="#exampleModal"></button>
-      
-      <!-- 모달 -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">제품번호 찾기</h5>
-            </div>
-            <div class="modal-body">
-              <!-- 제품명 입력 -->
-              <div class="border rounded p-3 mb-4">
-                <div class="row g-3">
-                  <div class="col-md-4"><label class="form-label">제품명</label><input type="text" class="form-control" v-model="vdr" />
-                    <button type="button" class="icon-btn" data-bs-toggle="modal"></button>
-                  </div>
-                </div>
-              </div>
-
-              <!-- 제품 리스트 테이블 -->
-              <table class="table table-bordered text-center align-middle">
-                <thead class="table-light">
-                  <tr>
-                    <th>선택</th>
-                    <th>제품번호</th>
-                    <th>제품명</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="prd in filterSpmInsStdList" v-bind:key="prd.prd_nm">
-                    <td><input type="radio"/></td>
-                    <td>{{ prd.prd_no }}</td>
-                    <td>{{ prd.prd_nm }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
-            <!-- FOOTER 버튼 -->
-            <div class="modal-footer">
-              <button type="button" class="btn btn-primary" @click="addPrd"> 선택</button>
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- 모달 끝-->
-
-      제품명 <input class="form-control" id="input" placeholder="" readonly style="background-color: #eee;" />
+      제품번호 <input v-model="searchQuery" class="form-control" id="input_id" placeholder=""  readonly/>
+        <button class="btn btn-outline-secondary" id="icon-btn" @click="openProductModal">🔍</button>
+      제품명 <input :value="selectedProductName" class="form-control" id="input"  readonly style="background-color: #eee;" />
+      <!-- 하단 데이터 입력-->
       작성자 <input class="form-control" id="input" placeholder="" readonly style="background-color: #eee;" />
       수정일자 <input class="form-control" placeholder="" readonly style="background-color: #eee;" />
     </div>
+
 
     <!-- 버튼 -->
     <div class="btn1">
@@ -78,36 +33,54 @@
         </tr>
       </thead>
 
-      <tbody>
-        <!-- 등록용 입력 라인 -->
-        <tr v-for="spm_ins_std in filterSpmInsStdList" v-bind:key="spm_ins_std.prd_no">
-          <td>{{ spm_ins_std.ins_itm }}</td>
-          <td>{{ spm_ins_std.ins_mthd }}</td>
-          <td>{{ spm_ins_std.ins_spc }}</td>
-          <td>{{ spm_ins_std.ins_eqp }}</td>
+      <tbody>   
+        <tr v-for="spm_ins_std in filterSpmInsStdList" v-bind:key="spm_ins_std_no">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
           <td></td>
         </tr>
       </tbody>
     </table>
   </div>
+
+  <PrdSelModal
+  v-if="showProductModal"
+  :prodList="prodList"
+  @select-product="handleSelectedProduct"
+  @close="showProductModal = false"
+/>
+
+    
 </template>
 
 <script>
 import axios from 'axios'
+import PrdSelModal from '@/views/qualitys/PrdSelModal.vue'
 
 export default {
+  components: { PrdSelModal },
   data() {
     return {
-      searchQuery: "",
+      searchQuery: "",             // 제품 ID
+      selectedProductName: "",     // 제품명
+      selectedProductPerson: "",   // 작성자
+      selectedProductDate: "",     // 수정일자
       selectedFilter: "",
       spmInsStdList: [],
-      isModalOpen: false,
+      showProductModal: false,
       form: {
         ins_itm: '',
         ins_mthd: '',
         ins_spc: '',
         ins_eqp: '',
+        ins_prd_nm: '',
+        crt_by: '', 
+        mdf_dt: '',
       },
+      showProductModal: false,
+      prodList: [],
     }
   },
   computed: {
@@ -127,11 +100,29 @@ export default {
       //this.form = result.data
     },
     showModal() {
-      this.isModalOpen = true
+      this.showProductModal = true
     },
     hideModal() {
-      this.isModalOpen = false
-    }
+      this.showProductModal = false
+    },
+    handleSelectedProduct(item) {
+      this.searchQuery = item.prd_no
+      this.selectedProductName = item.prd_nm
+      this.showProductModal = false
+    // 필요한 경우 이름도 같이 세팅
+    // this.selectedProductName = item.prd_nm
+  },
+    // 제품 선택 모달 열기
+    openProductModal() {
+      axios.get('/api/prodpln/prdList')
+        .then(res => {
+          this.prodList = res.data
+          this.showProductModal = true
+        })
+        .catch(err => {
+          console.error('제품 목록 불러오기 실패', err)
+        })
+    },
   },
 
 }
@@ -161,9 +152,12 @@ export default {
   float: right;
 }
 
+.input-group{
+  display: inline-block;
+  width: 130px;
+}
 
 .icon-btn {
-
   line-height: 30px;
   background-color: #ffffff;
   border: 1px solid #c9c9c9;
