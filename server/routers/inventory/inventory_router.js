8bb48e72ce0602cee8424b5b2ef02f4b692bea6e
882Eunae <pubao47@gmail.com>
2025-05-06 02:34:42 +0900

    자재구매계획 여러건 서버

--- server/routers/inventory/inventory_router.js
+++ server/routers/inventory/inventory_router.js
@@ -68,5 +68,49 @@ router.get('/inventory/minqty',async(req,res)=>{
   const minqty=await inventoryService.findMinqty(mat_no); 
   res.json(minqty); 
 })
+//출고번호 받은다음 어떤 자재 insert해야되는지 조회 
+// router.get('/inventory/addMat',async(req,res)=>{
+//   const reqNo=req.query.reqNo; 
+//   const addMat=await inventoryService.findAddMatByReqNo(reqNo);
+//   res.json(addMat);
+// }) 
+
+//자재구매계획 insert
+router.post('/inventory/addPlan',async(req,res)=>{
+ try{
+    const reqNo= (await inventoryService.findOldReqNo())[0]?.mat_req_no; 
+    console.log('reqNo',reqNo);
+    if (!reqNo) {
+      return res.status(200).json({ message: '처리할 출고요청이 없습니다.' });
+    }
+    //여기까지 실행잘됨 
+    //insert 할 자재목록 가져오기 
+    const addMatList=await inventoryService.findAddMatByReqNo(reqNo);
+    console.log('addMatList');
+    console.log(addMatList); 
+    for(const item of addMatList){
+      //MPP-??? 마지막 번호 찾기 
+      const lastMatNo = await inventoryService.findLastMatNo();
+      const nextMatNo=findNextCode(lastMatNo);
+      const formattedMatNo = `MPP-${String(nextMatNo).padStart(3, '0')}`;
+
+      await inventoryService.addMaterialPlan(formattedMatNo,item);
+      //자재 insert후 prc_rslt c3으로 변경 
+      await inventoryService.updatePr(reqNo,item.mat_no);
+      console.log('자재 insert 대상:', item);
+    
+    }
+    const remain=(await inventoryService.findMatReqPrRemain(reqNo))[0]?.remain; 
+    console.log('remain',remain); 
+    if(remain==0){
+      await inventoryService.changeMatReqSts(reqNo);
+    }
+    res.status(200).json({message:'자재구매계획 등록 완료',reqNo}); 
+  }catch(err){
+    console.log('오류',err);
+    res.status(500).json({message:'서버오류',error:err.message});  
+  }
+
+}) 
 
 module.exports=router;
